c678c5d21555ea1677df5b02f060b0bc3fd7b8e3
Tomohiro <tmitani@heroku.com>
2020-11-01 15:10:31 +0900

    update

--- spec/test_template_spec.rb
+++ spec/test_template_spec.rb
@@ -3,6 +3,6 @@ require_relative '../lib/test_template'
 
 describe 'The first test' do
   it 'should return expected value' do
-    expect(TestTemplate.new.test_method('hello world')).to eq('hello world')
+    expect(TestTemplate.new.test_method('hello world')).to eq 'hello world'
   end
 end
