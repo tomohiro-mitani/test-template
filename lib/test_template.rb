46db5791ceba9e47b4d05df0857bc2231a2779df
Tomohiro <tmitani@heroku.com>
2021-01-08 22:27:08 +0900

    test set

--- lib/test_template.rb
+++ lib/test_template.rb
@@ -2,8 +2,25 @@
 require 'pry'
 
 # This is a template for a script and a test
-class TestTemplate
-  def test_method(string)
-    string
+# Go through an array
+# each element and look up the key from the input
+# and if the value is smaller than minimum, set the minimum and return minumum
+class StripeInterview
+  def finding_min_record(table, columns)
+    return {} if table.empty? || column.empty?
+
+    negative_infinity = -1 / 0.0
+    min_row = 0
+    table.each_with_index do |record, row_number|
+      columns.each do |column|
+        min_value = negative_infinity if table[min_row][column].nil?
+        current_record_value = negative_infinity if record[column].nil?
+        if current_record_value != min_value
+          min_row = row_number if current_record_value < min_value
+          break
+        end
+      end
+    end
+    table[min_row]
   end
 end
--- spec/test_template_spec.rb
+++ spec/test_template_spec.rb
@@ -3,6 +3,9 @@ require_relative '../lib/test_template'
 
 describe 'The first test' do
   it 'should return expected value' do
-    expect(TestTemplate.new.test_method('hello world')).to eq 'hello world'
+    expect(StripeInterview.new.finding_min_record([{"a": 1, "b": -2, "d": -5, "x": -100},
+                                                   {"a": 1, "b": -3, "d": -5, "x": -100}],
+                                                  ["b", "d", "x"])).to
+                                                  eq ({"a": 1, "b": -2, "d": -5, "x": -100})
   end
 end
